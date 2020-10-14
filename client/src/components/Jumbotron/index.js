d7e38166b0ad494bf483f3a017e9c25d0dcfea18
Edward Reyes <reyes.edwardf@gmail.com>
2020-10-13 19:36:46 -0700

    wallpaper added

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,32 +3,35 @@ import { Link } from 'react-router-dom';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import recording from '../../assets/images/recording.jpg';
+import musicExchangeWallpaper from '../../assets/images/music-exchange-wallpaper.svg'
 
 
 const styles = {
   // backgroundColor: "transparent",
   height: "55vh",
   color: "white",
-  backgroundImage: `url('${recording}')`
+  backgroundImage: `url('${musicExchangeWallpaper}')`
 }
 
 const JumbotronComponent = () => {
   return (
-    <div>
+    <>
       <Jumbotron style={styles}>
-      <h1>Share your creativity!</h1>
-        <p>
-        Welcome to Music eXchange! Please explore the different user profiles to see what content each user has created!
+        <div style={{width: '30%'}}>
+          <h1>Share your creativity!</h1>
+          <p>
+            Welcome to Music eXchange! Please explore the different user profiles to see what content each user has created!
         </p>
-        <hr className="my-4" style={{ background: "white"}}/>
-        <p>
-        In each profile you can make a request to purchase a song, or to purchase a tutorial from the user!
+          <hr className="my-4" style={{ background: "white" }} />
+          <p>
+            In each profile you can make a request to purchase a song, or to purchase a tutorial from the user!
         </p>
-        <p>
-          <Button variant="outline-light">Learn more</Button>
-        </p>
+          <p>
+            <Button variant="outline-light">Learn more</Button>
+          </p>
+        </div>
       </Jumbotron>
-    </div>
+    </>
   )
 }
 
